17ff2577638e1524c9261f32f3690a4b457c86c9
EM <aslam4webz@gmail.com>
2019-05-11 01:40:38 +0530

    Updates

---
+++ precache-manifest.e98068704e64f26317485df68d01c79e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ba965165c21fd1151ff220eed750db",
+    "url": "/React-Movies/index.html"
+  },
+  {
+    "revision": "b5b14b94f270486aa48e",
+    "url": "/React-Movies/static/css/main.7ab9a7b2.chunk.css"
+  },
+  {
+    "revision": "1828e2aa2ccebcc6bbe0",
+    "url": "/React-Movies/static/js/2.6e2ef8cc.chunk.js"
+  },
+  {
+    "revision": "b5b14b94f270486aa48e",
+    "url": "/React-Movies/static/js/main.00aa1eb1.chunk.js"
+  },
+  {
+    "revision": "19d624ca67662084c7f4",
+    "url": "/React-Movies/static/js/runtime~main.e65bad4a.js"
+  }
+]);
